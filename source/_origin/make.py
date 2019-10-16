9fcb1703c3f9b649bcb1be5a61985ca56485f93a
iydon <1056736800@qq.com>
2019-10-16 19:09:20 +0800

    Revert "Update contents.rst as a test"
    This reverts commit b2bedaab04796d224dc704ed7835cd0c2113a05c.

--- source/_origin/make.py
+++ source/_origin/make.py
@@ -7,9 +7,6 @@ from utils import config, sh
 parse_macro_in(config.macros_file, cache=True)
 
 for file in sh.recursive_folder(ignores=config.ignore_dirs):
-	try:
-		content = parse_macro_in(file, cache=True)
-	except Exception as e:
-		print(e, file)
+	content = parse_macro_in(file, cache=True)
 	target = sh.join_path(config.target_path, file)
 	sh.write_file(target, content)
